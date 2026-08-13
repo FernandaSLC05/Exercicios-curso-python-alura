685a9e9c83834893930022c657f0f05625ccebb4
Fernanda Santa Lucia Corsi <fernandasantaluciac@gmail.com>
2026-08-12 23:09:33 -0300

    feat: exercicio 1

---
+++ Exercicio1.py
@@ -0,0 +1,17 @@
+
+# 1 - Crie uma lista para cada informação a seguir:
+
+# Lista de números de 1 a 10;
+# Lista com quatro nomes;
+# Lista com o ano que você nasceu e o ano atual.
+
+listaNumeros = [1,2,3,4,5,6,7,8,9,10]
+
+listaNomes = ["jonas", "cleber", "wuilson", "jubileu"]
+
+listaNiver = [2005, 2027]
+
+print (f"Lista de numeros: {listaNumeros}")
+print (f"Lista de nomes: {listaNomes}")
+print (f"Lista de datas: {listaNiver}")
+
